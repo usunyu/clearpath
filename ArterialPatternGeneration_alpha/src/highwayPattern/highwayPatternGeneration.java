0bd4016a1497f73a9b5fb4a37e8ffbb5789e8776
usunyu <usunyu@gmail.com>
2013-07-19 11:28:05 -0700

    highway pattern

--- ArterialPatternGeneration_alpha/src/highwayPattern/highwayPatternGeneration.java
+++ ArterialPatternGeneration_alpha/src/highwayPattern/highwayPatternGeneration.java
@@ -20,11 +20,15 @@ public class highwayPatternGeneration {
 	static String highwayLinkKML = "Highway_Link_List.kml";
 	static String highwaySensorKML = "Highway_Sensor_List.kml";
 	static String allHighwaySensorKML = "All_Highway_Sensor_List.kml";
+	static String averageSpeedFile = "Average_Speed_List.txt";
+	static String highwayPatternKML = "Highway_Pattern.kml";
 	// param
-	static double searchDistance = 0.05;
-	static double corssSearchDistance = 0.03;
-	static double noDirSearchDistance = 0.001;
+	static double searchDistance = 0.15;
+	static double corssSearchDistance = 0.15;
+	static double noDirSearchDistance = 0.01;
 	static int devide = 10;
+	static String[] days = { "Monday", "Tuesday", "Wednesday", "Thursday",
+			"Friday", "Saturday", "Sunday" };
 	// database
 	static String urlHome = "jdbc:oracle:thin:@geodb.usc.edu:1521/geodbs";
 	static String userName = "clearp";
@@ -42,6 +46,7 @@ public class highwayPatternGeneration {
 	static ArrayList<SensorInfo> sensorList = new ArrayList<SensorInfo>();
 	static ArrayList<SensorInfo> matchSensorList = new ArrayList<SensorInfo>();
 	static HashSet<Integer> checkMatchSensor = new HashSet<Integer>();
+	static HashMap<Integer, double[]> sensorSpeedPattern = new HashMap<Integer, double[]>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -51,15 +56,202 @@ public class highwayPatternGeneration {
 		readLinkFile();
 		fetchSensor();
 		matchLinkSensor();
-		generateLinkKML();
-		generateSensorKML();
+		// generateLinkKML();
+		// generateSensorKML();
 		// generateAllSensorKML();
+		// writeAverageFile(days[0]);
+		readAverageFile();
+		generatePatternKML(UtilClass.getStartTime(9));
+	}
+
+	private static void generatePatternKML(String time) {
+		System.out.println("generate pattern...");
+		try {
+			FileWriter fstream = new FileWriter(root + "/" + highwayPatternKML);
+			BufferedWriter out = new BufferedWriter(fstream);
+			out.write("<kml><Document>");
+			for (int i = 0; i < linkList.size(); i++) {
+				LinkInfo link = linkList.get(i);
+				int intId = link.getIntLinkId();
+				ArrayList<SensorInfo> sensorList = link.getSensorList();
+				String sensorStr = "null";
+				int sensorId = 0;
+
+				for (int j = 0; j < sensorList.size(); j++) {
+					if (j == 0) {
+						sensorId = sensorList.get(j).getSensorId();
+						sensorStr = String.valueOf(sensorId);
+					}
+					else {
+						sensorStr = sensorStr + "," + String.valueOf(sensorList.get(j).getSensorId());
+					}
+				}
+				
+				String patternStr = "null";
+				int showIndex = UtilClass.getIndex(time);
+				double[] speedArray = null;
+				
+				if(sensorId != 0) {
+					speedArray = sensorSpeedPattern.get(sensorId);
+					for(int j = 0; j < speedArray.length; j++) {
+						if(j == 0)
+							patternStr = UtilClass.getStartTime(j) + " " + speedArray[j];
+						else {
+							patternStr = patternStr + "\r\n" + UtilClass.getStartTime(j) + " " + speedArray[j];
+						}
+					}
+				}
+				
+				String colorStr = "#FFFFFFFF";
+				if(speedArray != null)
+					colorStr = getColor(speedArray[showIndex]);
+
+				ArrayList<PairInfo> nodeList = link.getNodeList();
+				String kmlStr = "<Placemark><name>Link:" + intId + "</name>";
+				kmlStr += "<description>";
+				kmlStr += "Sensor:" + sensorStr;
+				kmlStr += patternStr;
+				// kmlStr += ", Name:" + link.getSt_name();
+				// kmlStr += ", Start:" + link.getStartNode();
+				// kmlStr += ", End:" + link.getEndNode();
+				// kmlStr += ", Dir:" + link.getAllDir();
+				// kmlStr += ", Func:" + link.getFunc_class();
+				// kmlStr += ", Speed:" + link.getSpeedCat();
+				kmlStr += "</description>";
+				kmlStr += "<LineString><tessellate>1</tessellate><coordinates>";
+				for (int j = 0; j < nodeList.size(); j++) {
+					PairInfo node = nodeList.get(j);
+					kmlStr += node.getLongi() + "," + node.getLati() + ",0 ";
+				}
+				kmlStr += "</coordinates></LineString>";
+				kmlStr += "<Style><LineStyle>";
+				kmlStr += "<color>" + colorStr + "</color>";
+				kmlStr += "<width>5</width>";
+				kmlStr += "</LineStyle></Style></Placemark>\n";
+				out.write(kmlStr);
+			}
+			out.write("</Document></kml>");
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("generate pattern finish!");
+	}
+	
+	private static String getColor(double speed) {
+		if(speed < 30)
+			return "#FF0000FF";
+		else if(speed >= 30 && speed < 55)
+			return "#FFE11EFF";
+		else if(speed >= 55)
+			return "#0DFF82FF";
+		return "#FFFFFFFF";
+	}
+
+	private static void readAverageFile() {
+		System.out.println("read average file...");
+		try {
+			FileInputStream fstream = new FileInputStream(root + "/"
+					+ averageSpeedFile);
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			while ((strLine = br.readLine()) != null) {
+				String[] nodes = strLine.split(";");
+				int sensorId = Integer.parseInt(nodes[0]);
+				double speed = Double.parseDouble(nodes[1]);
+				String time = nodes[2];
+
+				if (sensorSpeedPattern.containsKey(sensorId)) {
+					double[] tempArray = sensorSpeedPattern.get(sensorId);
+					int index = UtilClass.getIndex(time);
+					if (index >= 0 && index <= 59)
+						tempArray[index] = speed;
+				} else {
+					double[] newArray = new double[60];
+					int index = UtilClass.getIndex(time);
+					if (index >= 0 && index <= 59)
+						newArray[index] = speed;
+					sensorSpeedPattern.put(sensorId, newArray);
+				}
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		System.out.println("read average file finish!");
+	}
+
+	private static void writeAverageFile(String day) {
+		System.out.println("fetch average speed...");
+		try {
+			FileWriter fstream = new FileWriter(root + "/" + averageSpeedFile);
+			BufferedWriter out = new BufferedWriter(fstream);
+
+			Connection con = null;
+			String sql = null;
+			PreparedStatement pstatement = null;
+			ResultSet res = null;
+			con = getConnection();
+			sql = "SELECT * FROM highway_averages3_new4 WHERE day = '" + day
+					+ "'";
+			pstatement = con.prepareStatement(sql,
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			res = pstatement.executeQuery();
+			int i = 0;
+			while (res.next()) {
+				int sensorId = res.getInt(1);
+				double speed = res.getDouble(2);
+				String time = res.getString(5);
+
+				if (sensorSpeedPattern.containsKey(sensorId)) {
+					double[] tempArray = sensorSpeedPattern.get(sensorId);
+					int index = UtilClass.getIndex(time);
+					if (index >= 0 && index <= 59)
+						tempArray[index] = speed;
+				} else {
+					double[] newArray = new double[60];
+					int index = UtilClass.getIndex(time);
+					if (index >= 0 && index <= 59)
+						newArray[index] = speed;
+					sensorSpeedPattern.put(sensorId, newArray);
+				}
+
+				String strLine = res.getString(1) + ";" + res.getString(2)
+						+ ";" + time + "\r\n";
+				out.write(strLine);
+
+				if (i % 1000 == 0)
+					System.out.println((float) i / 121920 * 100 + "%");
+				i++;
+			}
+
+			out.close();
+
+			res.close();
+			pstatement.close();
+			con.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		System.out.println("fetch average speed finish!");
 	}
 
 	private static void matchLinkSensor() {
 		// 3 round match algorithm
 		System.out.println("match sensors to links...");
 		// 1) match same direction
+		firstRoundMatch();
+		// 2) match direction 0 to 3, 1 to 2
+		secondRoundMatch();
+		// 3) match smallest distance
+		// thirdRoundMatch();
+		System.out.println("match Sensors finish!");
+	}
+
+	private static void firstRoundMatch() {
 		System.out.println("first round...");
 		for (int i = 0; i < linkList.size(); i++) {
 			LinkInfo link = linkList.get(i);
@@ -95,10 +287,13 @@ public class highwayPatternGeneration {
 					}
 				}
 			}
-			if (i % 1000 == 0)
+			if (i % 1000 == 0 || i == linkList.size())
 				System.out.println((float) i / linkList.size() * 100 + "%");
 		}
-		// 2) match direction 0 to 3, 1 to 2
+		System.out.println("first round finish!");
+	}
+
+	private static void secondRoundMatch() {
 		System.out.println("second round...");
 		for (int i = 0; i < linkList.size(); i++) {
 			LinkInfo link = linkList.get(i);
@@ -143,10 +338,13 @@ public class highwayPatternGeneration {
 					}
 				}
 			}
-			if (i % 1000 == 0)
+			if (i % 1000 == 0 || i == linkList.size())
 				System.out.println((float) i / linkList.size() * 100 + "%");
 		}
-		// 3) match smallest distance
+		System.out.println("second round finish!");
+	}
+
+	private static void thirdRoundMatch() {
 		System.out.println("third round...");
 		for (int i = 0; i < linkList.size(); i++) {
 			LinkInfo link = linkList.get(i);
@@ -175,10 +373,10 @@ public class highwayPatternGeneration {
 					}
 				}
 			}
-			if (i % 1000 == 0)
+			if (i % 1000 == 0 || i == linkList.size())
 				System.out.println((float) i / linkList.size() * 100 + "%");
 		}
-		System.out.println("match Sensors finish!");
+		System.out.println("third round finish!");
 	}
 
 	private static void generateSensorKML() {
@@ -235,7 +433,7 @@ public class highwayPatternGeneration {
 	}
 
 	private static void readLinkFile() {
-		System.out.println("fetch link file...");
+		System.out.println("read link file...");
 		try {
 			FileInputStream fstream = new FileInputStream(root + "/"
 					+ highwayLinkFile);
@@ -277,7 +475,7 @@ public class highwayPatternGeneration {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
-		System.out.println("fetch link file finish!");
+		System.out.println("read link file finish!");
 	}
 
 	private static void fetchSensor() {
@@ -346,11 +544,12 @@ public class highwayPatternGeneration {
 				kmlStr += "<description>";
 				kmlStr += "Sensor:" + sensorStr;
 				kmlStr += ", Name:" + link.getSt_name();
-				kmlStr += ", Start:" + link.getStartNode();
-				kmlStr += ", End:" + link.getEndNode();
+				// kmlStr += ", Start:" + link.getStartNode();
+				// kmlStr += ", End:" + link.getEndNode();
 				kmlStr += ", Dir:" + link.getAllDir();
-				kmlStr += ", Func:" + link.getFunc_class();
-				kmlStr += ", Speed:" + link.getSpeedCat() + "</description>";
+				// kmlStr += ", Func:" + link.getFunc_class();
+				// kmlStr += ", Speed:" + link.getSpeedCat();
+				kmlStr += "</description>";
 				kmlStr += "<LineString><tessellate>1</tessellate><coordinates>";
 				for (int j = 0; j < nodeList.size(); j++) {
 					PairInfo node = nodeList.get(j);
