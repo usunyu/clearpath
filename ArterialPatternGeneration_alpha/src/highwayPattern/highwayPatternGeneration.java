241cf1b057633bba8e00456625a08fb323b55187
usunyu <usunyu@gmail.com>
2013-07-18 18:10:32 -0700

    match first

--- ArterialPatternGeneration_alpha/src/highwayPattern/highwayPatternGeneration.java
+++ ArterialPatternGeneration_alpha/src/highwayPattern/highwayPatternGeneration.java
@@ -14,93 +14,197 @@ public class highwayPatternGeneration {
 	/**
 	 * @param args
 	 */
+	// file
+	static String root = "file";
+	static String highwayLinkFile = "Highway_Link_List.txt";
+	static String highwayLinkKML = "Highway_Link_List.kml";
+	static String highwaySensorKML = "Highway_Sensor_List.kml";
+	// param
+	static double searchDistance = 0.05;
+	static int devide = 10;
 	// database
 	static String urlHome = "jdbc:oracle:thin:@geodb.usc.edu:1521/geodbs";
 	static String userName = "clearp";
 	static String password = "clearp";
 	static Connection connHome = null;
 	// data struct
-	static ArrayList<String> highwayList = new ArrayList<String>();
-	static HashMap<String, ArrayList<LinkInfo>> highwayLinkMap = new HashMap<String, ArrayList<LinkInfo>>();
-
+	// link
 	static ArrayList<Integer> linkIdList = new ArrayList<Integer>();
 	static ArrayList<LinkInfo> linkList = new ArrayList<LinkInfo>();
 	static HashMap<Integer, LinkInfo> linkMap = new HashMap<Integer, LinkInfo>();
-	static HashMap<Integer, PairInfo> nodePosition = new HashMap<Integer, PairInfo>();
+	// node
+	static HashMap<Integer, PairInfo> nodePositionMap = new HashMap<Integer, PairInfo>();
+	// sensor
+	static HashSet<Integer> checkSensor = new HashSet<Integer>();
+	static ArrayList<SensorInfo> sensorList = new ArrayList<SensorInfo>();
+	static ArrayList<SensorInfo> matchSensorList = new ArrayList<SensorInfo>();
+	static HashSet<Integer> checkMatchSensor = new HashSet<Integer>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		fetchHighwayName();
-		fetchHighwayLink();
-		/* ------------------------- */
 		// fetchLinkId();
 		// fetchLink();
-		// generateLinkKML();
 		// writeLinkFile();
-		// fetchSensor();
+		readLinkFile();
+		fetchSensor();
+		matchLinkSensor();
+		generateLinkKML();
+		generateSensorKML();
 	}
 	
-	private static void fetchHighwayLink() {
-		System.out.println("fetch highway link...");
+	private static void matchLinkSensor() {
+		System.out.println("match sensors to links...");
+		for (int i = 0; i < linkList.size(); i++) {
+			LinkInfo link = linkList.get(i);
+			ArrayList<PairInfo> nodeList = link.getNodeList();
+			for (double step = searchDistance / devide; step < searchDistance; step += step) {
+				for (int j = 0; j < nodeList.size(); j++) {
+					PairInfo node1 = nodeList.get(j);
+					for (int k = 0; k < sensorList.size(); k++) {
+						SensorInfo sensor = sensorList.get(k);
+						PairInfo node2 = sensor.getNode();
+						// same direction
+						String allDir = link.getAllDir();
+						String[] dirList = allDir.split(",");
+						for(int d = 0; d < dirList.length; d++) {
+							if(sensor.getDirection() == Integer.parseInt(dirList[d])) {
+								double distance = DistanceCalculator.CalculationByDistance(node1, node2);
+								// in the search area
+								if (distance < step) {
+									// match sensor
+									if(!link.containSensor(sensor))
+										link.addSensor(sensor);
+									if(!checkMatchSensor.contains(sensor.getSensorId())) {
+										matchSensorList.add(sensor);
+										checkMatchSensor.add(sensor.getSensorId());
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			if(i % 100 == 0)
+				System.out.println((float)i / linkList.size() * 100 + "%");
+		}
+		System.out.println("match Sensors finish!");
+	}
+	
+	private static void generateSensorKML() {
+		System.out.println("generate sensor kml...");
 		try {
-			Connection con = null;
-			String sql = null;
-			PreparedStatement pstatement = null;
-			ResultSet res = null;
-			con = getConnection();
-			for(int i = 0; i < highwayList.size(); i++) {
-				
+			FileWriter fstream = new FileWriter(root + "/" + highwaySensorKML);
+			BufferedWriter out = new BufferedWriter(fstream);
+			out.write("<kml><Document>");
+			for (int i = 0; i < matchSensorList.size(); i++) {
+				SensorInfo sensor = matchSensorList.get(i);
+				int id = sensor.getSensorId();
+				double lati = sensor.getNode().getLati();
+				double longi = sensor.getNode().getLongi();
+				out.write("<Placemark><name>" + id + "</name><description>On:"
+						+ sensor.getOnStreet() + ", From: "
+						+ sensor.getFromStreet() + ", Dir:"
+						+ sensor.getDirection()
+						+ "</description><Point><coordinates>" + longi + ","
+						+ lati + ",0</coordinates></Point></Placemark>");
+			}
+			out.write("</Document></kml>");
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("generate sensor kml finish!");
+	}
+
+	private static void readLinkFile() {
+		System.out.println("fetch link file...");
+		try {
+			FileInputStream fstream = new FileInputStream(root + "/"
+					+ highwayLinkFile);
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			while ((strLine = br.readLine()) != null) {
+				String[] nodes = strLine.split(";");
+				int linkId = Integer.parseInt(nodes[0]);
+				String dir = nodes[1];
+				String name = nodes[2];
+				int funClass = Integer.parseInt(nodes[3]);
+				String nodeListString = nodes[4];
+				ArrayList<PairInfo> nodeList = new ArrayList<PairInfo>();
+				String[] nodeString = nodeListString.split(":");
+				for (int i = 0; i < nodeString.length; i++) {
+					String[] loc = nodeString[i].split(",");
+					PairInfo node = new PairInfo(Double.parseDouble(loc[1]),
+							Double.parseDouble(loc[0]));
+					nodeList.add(node);
+				}
+				int speedCat = Integer.parseInt(nodes[5]);
+				String dirTravel = nodes[6];
+				int refNode = Integer.parseInt(nodes[7]);
+				int nrefNode = Integer.parseInt(nodes[8]);
+
+				if (!nodePositionMap.containsKey(refNode))
+					nodePositionMap.put(refNode, nodeList.get(0));
+				if (!nodePositionMap.containsKey(nrefNode))
+					nodePositionMap.put(nrefNode, nodeList.get(1));
+
+				LinkInfo link = new LinkInfo(linkId, funClass, name, refNode,
+						nrefNode, nodeList, dirTravel, speedCat, dir);
+				linkList.add(link);
+				linkMap.put(linkId, link);
+
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
-		System.out.println("fetch highway link finish!");
+		System.out.println("fetch link file finish!");
 	}
 
-	private static void fetchHighwayName() {
-		System.out.println("fetch highway name...");
+	private static void fetchSensor() {
+		System.out.println("fetch sensor...");
 		try {
 			Connection con = null;
 			String sql = null;
 			PreparedStatement pstatement = null;
 			ResultSet res = null;
 			con = getConnection();
-
-			sql = "SELECT DISTINCT st_name FROM streets_dca1_new WHERE func_class = 1 OR func_class = 2";
-
+			sql = "select link_id, onstreet, fromstreet, start_lat_long, direction from highway_congestion_config ";
 			pstatement = con.prepareStatement(sql,
 					ResultSet.TYPE_SCROLL_INSENSITIVE,
 					ResultSet.CONCUR_READ_ONLY);
 			res = pstatement.executeQuery();
-
 			while (res.next()) {
-				String highwayNmae = res.getString(1);
-				if (highwayNmae == null)
-					continue;
-				highwayList.add(highwayNmae);
+				int sensorId = res.getInt(1);
+				// System.out.println("fetching sensor " + sensorId);
+				String onStreet = res.getString(2);
+				String fromStreet = res.getString(3);
+				STRUCT st = (STRUCT) res.getObject(4);
+				JGeometry geom = JGeometry.load(st);
+				PairInfo node = new PairInfo(geom.getPoint()[1],
+						geom.getPoint()[0]);
+				int direction = res.getInt(5);
+				if (!checkSensor.contains(sensorId)) {
+					SensorInfo sensorInfo = new SensorInfo(sensorId, onStreet,
+							fromStreet, node, direction);
+					checkSensor.add(sensorId);
+					sensorList.add(sensorInfo);
+				}
 			}
 			res.close();
 			pstatement.close();
 			con.close();
 		} catch (Exception e) {
-			// TODO: handle exception
 			e.printStackTrace();
 		}
-		System.out.println("fetch highway name finish!");
-	}
-
-	private static void fetchSensor() {
-		System.out.println("fetch Sensor...");
-
 		System.out.println("fetch sensor finish!");
 	}
 
-	/* -------------------------------------------------- */
 	private static void generateLinkKML() {
 		System.out.println("generate link kml...");
 		try {
-			FileWriter fstream = new FileWriter("Highway_Link_List.kml");
+			FileWriter fstream = new FileWriter(root + "/" + highwayLinkKML);
 			BufferedWriter out = new BufferedWriter(fstream);
 			out.write("<kml><Document>");
 			for (int i = 0; i < linkList.size(); i++) {
@@ -119,9 +223,9 @@ public class highwayPatternGeneration {
 				kmlStr += ", Name:" + link.getSt_name();
 				kmlStr += ", Start:" + link.getStartNode();
 				kmlStr += ", End:" + link.getEndNode();
-				kmlStr += ", Direction:" + link.getAllDir();
-				kmlStr += ", FuncClass:" + link.getFunc_class();
-				kmlStr += ", SpeedCat:" + link.getSpeedCat() + "</description>";
+				kmlStr += ", Dir:" + link.getAllDir();
+				kmlStr += ", Func:" + link.getFunc_class();
+				kmlStr += ", Speed:" + link.getSpeedCat() + "</description>";
 				kmlStr += "<LineString><tessellate>1</tessellate><coordinates>";
 				for (int j = 0; j < nodeList.size(); j++) {
 					PairInfo node = nodeList.get(j);
@@ -142,21 +246,29 @@ public class highwayPatternGeneration {
 	private static void writeLinkFile() {
 		System.out.println("write link file...");
 		try {
-			FileWriter fstream = new FileWriter("Highway_Link_List.txt");
+			FileWriter fstream = new FileWriter(root + "/" + highwayLinkFile);
 			BufferedWriter out = new BufferedWriter(fstream);
 			for (int i = 0; i < linkList.size(); i++) {
 				LinkInfo link = linkList.get(i);
 				ArrayList<PairInfo> nodeList = link.getNodeList();
 				int num = nodeList.size();
-				String str = Integer.toString(link.getIntLinkId()) + "|"
-						+ link.getAllDir() + "|" + link.getSt_name() + "|"
-						+ link.getFunc_class() + "|"
-						+ nodeList.get(0).getLongi() + ","
-						+ nodeList.get(0).getLati() + "|"
-						+ nodeList.get(num - 1).getLongi() + ","
-						+ nodeList.get(num - 1).getLati() + "|"
-						+ link.getSpeedCat() + "|" + link.getStartNode() + "|"
-						+ link.getEndNode() + "\r\n";
+				String str = Integer.toString(link.getIntLinkId()) + ";";
+				str = str + link.getAllDir() + ";" + link.getSt_name() + ";";
+				str = str + link.getFunc_class() + ";";
+				String nodeListString = "";
+				for (int j = 0; j < num; j++) {
+					String nodeString = nodeList.get(j).getLongi() + ","
+							+ nodeList.get(j).getLati();
+					if (nodeListString.equals(""))
+						nodeListString = nodeString;
+					else
+						nodeListString = nodeListString + ":" + nodeString;
+				}
+				str = str + nodeListString + ";";
+				str = str + link.getSpeedCat() + ";";
+				str = str + link.getDirTravel() + ";";
+				str = str + link.getStartNode() + ";";
+				str = str + link.getEndNode() + "\r\n";
 				out.write(str);
 			}
 			out.close();
@@ -214,10 +326,10 @@ public class highwayPatternGeneration {
 
 				int num = nodeList.size();
 
-				if (!nodePosition.containsKey(refNode))
-					nodePosition.put(refNode, nodeList.get(0));
-				if (!nodePosition.containsKey(nrefNode))
-					nodePosition.put(nrefNode, nodeList.get(num - 1));
+				if (!nodePositionMap.containsKey(refNode))
+					nodePositionMap.put(refNode, nodeList.get(0));
+				if (!nodePositionMap.containsKey(nrefNode))
+					nodePositionMap.put(nrefNode, nodeList.get(num - 1));
 
 				if (dirTravel.equals("B")) {
 					int dir1 = DistanceCalculator.getDirection(nodeList.get(0),
