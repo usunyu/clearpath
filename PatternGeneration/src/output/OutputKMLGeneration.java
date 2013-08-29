9a0fe51f7b64f48c5f88b114db53447a09bf2987
usunyu <usunyu@gmail.com>
2013-08-29 16:24:39 -0700

    fix pattern generation kml

--- PatternGeneration/src/output/OutputKMLGeneration.java
+++ PatternGeneration/src/output/OutputKMLGeneration.java
@@ -47,43 +47,43 @@ public class OutputKMLGeneration {
 			for (int i = 0; i < CALinkList.size(); i++) {
 				CALinkInfo link = CALinkList.get(i);
 				int linkId = link.getLinkId();
-				int networkId = link.getNetworkId();
+				//int networkId = link.getNetworkId();
 				int linkClass = link.getLinkClass();
-				boolean rampFlag = link.getRampFlag();
-				boolean internalFlag = link.getInternalFlag();
-				boolean activeFlag = link.getActiveFlag();
+				//boolean rampFlag = link.getRampFlag();
+				//boolean internalFlag = link.getInternalFlag();
+				//boolean activeFlag = link.getActiveFlag();
 				int fromNodeId = link.getFromNodeId();
 				int toNodeId = link.getToNodeId();
-				double linkLengthKm = link.getLinkLengthKm();
-				int primaryRoadwayId = link.getPrimaryRoadwayId();
-				String linkDesc = link.getLinkDesc();
-				String fromDesc = link.getFromDesc();
-				String toDesc = link.getToDesc();
-				double speedLimitKmh = link.getSpeedLimitKmh();
-				PairInfo startLoc = link.getStartLoc();
-				PairInfo endLoc = link.getEndLoc();
-				PairInfo minLoc = link.getMinLoc();
-				PairInfo maxLoc = link.getMaxLoc();
+				//double linkLengthKm = link.getLinkLengthKm();
+				//int primaryRoadwayId = link.getPrimaryRoadwayId();
+				//String linkDesc = link.getLinkDesc();
+				//String fromDesc = link.getFromDesc();
+				//String toDesc = link.getToDesc();
+				//double speedLimitKmh = link.getSpeedLimitKmh();
+				//PairInfo startLoc = link.getStartLoc();
+				//PairInfo endLoc = link.getEndLoc();
+				//PairInfo minLoc = link.getMinLoc();
+				//PairInfo maxLoc = link.getMaxLoc();
 				ArrayList<PairInfo> pathPoints = link.getPathPoints();
 				// String encodedPolyline;
-				double fromProjCompassAngle = link.getFromProjCompassAngle();
-				double toProjCompassAngle = link.getToProjCompassAngle();
-				String sourceId = link.getSourceId();
-				String sourceRef = link.getSourceRef();
+				//double fromProjCompassAngle = link.getFromProjCompassAngle();
+				//double toProjCompassAngle = link.getToProjCompassAngle();
+				//String sourceId = link.getSourceId();
+				//String sourceRef = link.getSourceRef();
 				String tmcCode = link.getTmcCode();
 
 				String kmlStr = "<Placemark><name>Link:" + linkId + "</name>";
 				kmlStr += "<description>";
 				// kmlStr += "Network:" + networkId + "\r\n";
-				// kmlStr += "Class:" + linkClass + "\r\n";
+				kmlStr += "Class:" + linkClass + "\r\n";
 				// kmlStr += "Ramp:" + rampFlag + "\r\n";
 				// kmlStr += "Internal:" + internalFlag + "\r\n";
 				// kmlStr += "Active:" + activeFlag + "\r\n";
-				// kmlStr += "FromNode:" + fromNodeId + "\r\n";
-				// kmlStr += "ToNode:" + toNodeId + "\r\n";
+				kmlStr += "FromNode:" + fromNodeId + "\r\n";
+				kmlStr += "ToNode:" + toNodeId + "\r\n";
 				// kmlStr += "Length(Km):" + linkLengthKm + "\r\n";
 				// kmlStr += "Primary:" + primaryRoadwayId + "\r\n";
-				kmlStr += "LinkDesc:" + linkDesc + "\r\n";
+				// kmlStr += "LinkDesc:" + linkDesc + "\r\n";
 				// kmlStr += "FromDesc:" + fromDesc + "\r\n";
 				// kmlStr += "ToDesc:" + toDesc + "\r\n";
 				// kmlStr += "SpeedLimit(Km/h):" + speedLimitKmh + "\r\n";
@@ -123,37 +123,31 @@ public class OutputKMLGeneration {
 				debug++;
 				String[] nodes = strLine.split("\\|\\|");
 				int linkId = Integer.parseInt(nodes[0]);
-				int networkId = Integer.parseInt(nodes[1]);
-				int linkClass = Integer.parseInt(nodes[2]);
-				boolean rampFlag = getBooleanFromStr(nodes[3]);
-				boolean internalFlag = getBooleanFromStr(nodes[4]);
-				boolean activeFlag = getBooleanFromStr(nodes[5]);
-				int fromNodeIdNew = Integer.parseInt(nodes[6]);
-				int toNodeIdNew = Integer.parseInt(nodes[7]);
-				double linkLengthKm = Double.parseDouble(nodes[8]);
-				int primaryRoadwayId = Integer.parseInt(nodes[9]);
-				String linkDesc = nodes[10];
-				String fromDesc = nodes[11];
-				String toDesc = nodes[12];
-				double speedLimitKmh = Double.parseDouble(nodes[13]);
-				PairInfo startLoc = getPairFromStr(nodes[14]);
-				PairInfo endLoc = getPairFromStr(nodes[15]);
-				PairInfo minLoc = getPairFromStr(nodes[16]);
-				PairInfo maxLoc = getPairFromStr(nodes[17]);
-				ArrayList<PairInfo> pathPoints = getPairListFromStr(nodes[18]);
-				double fromProjCompassAngle = Double.parseDouble(nodes[19]);
-				double toProjCompassAngle = Double.parseDouble(nodes[20]);
-				String sourceId = nodes[21];
-				String sourceRef = nodes[22];
-				String tmcCode = nodes[23];
+				//int networkId = Integer.parseInt(nodes[1]);
+				int linkClass = Integer.parseInt(nodes[1]);
+				//boolean rampFlag = getBooleanFromStr(nodes[3]);
+				//boolean internalFlag = getBooleanFromStr(nodes[4]);
+				//boolean activeFlag = getBooleanFromStr(nodes[5]);
+				int fromNodeIdNew = Integer.parseInt(nodes[2]);
+				int toNodeIdNew = Integer.parseInt(nodes[3]);
+				//double linkLengthKm = Double.parseDouble(nodes[8]);
+				//int primaryRoadwayId = Integer.parseInt(nodes[9]);
+				//String linkDesc = nodes[10];
+				//String fromDesc = nodes[11];
+				//String toDesc = nodes[12];
+				//double speedLimitKmh = Double.parseDouble(nodes[13]);
+				PairInfo startLoc = getPairFromStr(nodes[4]);
+				PairInfo endLoc = getPairFromStr(nodes[5]);
+				//PairInfo minLoc = getPairFromStr(nodes[16]);
+				//PairInfo maxLoc = getPairFromStr(nodes[17]);
+				ArrayList<PairInfo> pathPoints = getPairListFromStr(nodes[6]);
+				//double fromProjCompassAngle = Double.parseDouble(nodes[19]);
+				//double toProjCompassAngle = Double.parseDouble(nodes[20]);
+				//String sourceId = nodes[21];
+				//String sourceRef = nodes[22];
+				String tmcCode = nodes[7];
 
-				CALinkInfo CALink = new CALinkInfo(linkId, networkId,
-					linkClass, rampFlag, internalFlag, activeFlag,
-					fromNodeIdNew, toNodeIdNew, linkLengthKm, primaryRoadwayId,
-					linkDesc, fromDesc, toDesc, speedLimitKmh, startLoc,
-					endLoc, minLoc, maxLoc, pathPoints,
-					fromProjCompassAngle, toProjCompassAngle, sourceId,
-					sourceRef, tmcCode);
+				CALinkInfo CALink = new CALinkInfo(linkId, linkClass, fromNodeIdNew, toNodeIdNew, startLoc, endLoc, pathPoints, tmcCode);
 
 				CALinkList.add(CALink);
 				
@@ -205,21 +199,19 @@ public class OutputKMLGeneration {
 				String[] nodes = strLine.split("\\|\\|");
 				int nodeId = Integer.parseInt(nodes[0]);
 				int newNodeId = Integer.parseInt(nodes[1]);
-				int networkId = Integer.parseInt(nodes[2]);
-				String nodeType = nodes[3];
-				int minLinkClass = Integer.parseInt(nodes[4]);
-				String nodeName = nodes[5];
-				String locationStr = nodes[6];
+				//int networkId = Integer.parseInt(nodes[2]);
+				//String nodeType = nodes[3];
+				//int minLinkClass = Integer.parseInt(nodes[4]);
+				//String nodeName = nodes[5];
+				String locationStr = nodes[2];
 				String[] locNode = locationStr.split(",");
 				double lat = Double.parseDouble(locNode[0]);
 				double lng = Double.parseDouble(locNode[1]);
 				PairInfo location = new PairInfo(lat, lng);
-				String sourceId1 = nodes[7];
-				String sourceRef1 = nodes[8];
+				//String sourceId1 = nodes[7];
+				//String sourceRef1 = nodes[8];
 				
-				CANodeInfo CANode = new CANodeInfo(nodeId, newNodeId,
-					networkId, nodeType, minLinkClass, nodeName, location,
-					sourceId1, sourceRef1);
+				CANodeInfo CANode = new CANodeInfo(nodeId, newNodeId, location);
 				
 				CANodeList.add(CANode);
 				oldToNewNodeMap.put(nodeId, newNodeId);
